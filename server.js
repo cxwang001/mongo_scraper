ca3b1ab471881711157e7c6893f1990ffc8b2e62
cxwang001 <william@Williams-MBP.attlocal.net>
2017-08-11 23:47:21 -0700

    Heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-/* Showing Mongoose's "Populated" Method
- * =============================================== */
-
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 8080;
+
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Requiring our Note and Article models
@@ -12,8 +13,9 @@ var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-var exphbs = require("express-handlebars");
-// Set mongoose to leverage built in JavaScript ES6 Promises
+// Mongoose mpromise deprecated - use bluebird promises
+var Promise = require("bluebird");
+
 mongoose.Promise = Promise;
 
 
@@ -25,15 +27,28 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// Make public a static dir
-app.use(express.static("public"));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
 
 // Database configuration with mongoose
 // mongoose.connect("mongodb://localhost/mongo_scraper");
 
+// mongoose.connect("mongodb://heroku_tds6xj2t:tros3llla8me2tpriofhl7td1j@ds157459.mlab.com:57459/heroku_tds6xj2t");
+var db = mongoose.connection;
+// Make public a static dir
+// app.use(express.static("public"));
 
+// Database configuration with mongoose
 var databaseUri = "mongodb://localhost/mongo_scraper";
 
 if (process.env.MONGODB_URI) {
@@ -42,9 +57,7 @@ if (process.env.MONGODB_URI) {
  });
 } else {
   mongoose.connect(databaseUri, {useMongoClient: true}); 
-
-var db = mongoose.connection;
-
+}
 // Show any mongoose errors
 db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
@@ -55,25 +68,35 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-
 // Routes
 // ======
 
-// A GET request to scrape the echojs website
+// Simple index route
+app.get("/", function(req, res) {
+  Article.find({}).populate('note').exec(function(error, doc) {
+    var hbsObject = {
+      article: doc
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+// A GET request to scrape the website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  request("http://www.nytimes.com", function(error, response, html) {
+  request("https://www.reddit.com/r/webdev", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
-    $("h2.story-heading").each(function(i, element) {
+    $("p.title").each(function(i, element) {
 
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
+      result.title = $(this).text();
+      result.link = $(this).children().attr("href");
 
       // Using our Article model, create a new entry
       // This effectively passes the result object to the entry (and the title and link)
@@ -90,26 +113,25 @@ app.get("/scrape", function(req, res) {
           console.log(doc);
         }
       });
-
     });
   });
   // Tell the browser that we finished scraping the text
+  // res.send("Scrape complete!");
   res.redirect('/');
- 
 });
 
+
 // This will get the articles we scraped from the mongoDB
-app.get("/", function(req, res) {
+app.get("/articles", function(req, res) {
   // Grab every doc in the Articles array
-  Article.find({}, function(error, doc) {
+  Article.find({}).populate('note').exec(function(error, doc) {
     // Log any errors
     if (error) {
       console.log(error);
     }
+    // Or send the doc to the browser as a json object
     else {
-     
-    res.render("index", {article: doc});
-
+       res.json(doc);
     }
   });
 });
@@ -133,9 +155,8 @@ app.get("/articles/:id", function(req, res) {
   });
 });
 
-
 // Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id/notes", function(req, res) {
   // Create a new note and pass the req.body to the entry
   var newNote = new Note(req.body);
 
@@ -145,10 +166,13 @@ app.post("/articles/:id", function(req, res) {
     if (error) {
       console.log(error);
     }
+    //else 
+    //  res.send(doc);
     // Otherwise
+    
     else {
       // Use the article id to find and update it's note
-      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      Article.findOneAndUpdate({ "_id": req.params.id }, { $push: { "note": doc._id } }, { new: true })
       // Execute the above query
       .exec(function(err, doc) {
         // Log any errors
@@ -157,15 +181,31 @@ app.post("/articles/:id", function(req, res) {
         }
         else {
           // Or send the document to the browser
-          res.send(doc);
-        }
+          // res.send(doc);
+          res.redirect('/');
+        }      
       });
-    }
+    }  
   });
 });
 
+app.delete("/notes/:id", function(req, res) {
+  console.log("id: " + req.params.id)
+  Note.remove({"_id": req.params.id})
+  // Execute the above query
+  .exec(function(err, doc) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+    }
+    else {
+    // Or send the document to the browser
+      res.redirect('/');
+    }
+  });
+})
 
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// Listen on port 8080
+app.listen(8080, function() {
+  console.log("App running on port 8080!");
 });
